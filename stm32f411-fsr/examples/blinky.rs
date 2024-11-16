a6d0062345d548d1adb0922da1aedea30b2e4330
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2024-11-17 01:03:14 +0200

    fix: make blinky work

--- stm32f411-fsr/examples/blinky.rs
+++ stm32f411-fsr/examples/blinky.rs
@@ -7,11 +7,12 @@ use panic_probe as _;
 
 #[rtic::app(device = stm32f4xx_hal::pac, peripherals = true)]
 mod app {
+    use rtt_target::{rprintln, rtt_init_print};
     use stm32f4xx_hal::{
         gpio::{Output, PushPull, PC13},
         pac,
         prelude::*,
-        timer::{Event, Timer},
+        timer::{CounterHz, Event, Timer},
     };
 
     #[shared]
@@ -21,11 +22,14 @@ mod app {
     #[local]
     struct Local {
         led: PC13<Output<PushPull>>,
-        timer: Timer<pac::TIM2>,
+        timer: CounterHz<pac::TIM2>,
     }
 
     #[init]
     fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+        rtt_init_print!();
+        rprintln!("hello world");
+
         // Get the device peripherals
         let device: pac::Peripherals = ctx.device;
 
@@ -33,12 +37,15 @@ mod app {
         let rcc = device.RCC.constrain();
         let clocks = rcc.cfgr.sysclk(84.MHz()).freeze();
 
-        // Initialize GPIOA (for the onboard LED on PA5)
+        // Initialize GPIOC (for the onboard LED on PC13)
         let gpioc = device.GPIOC.split();
-        let led = gpioc.pc13.into_push_pull_output();
+        let mut led = gpioc.pc13.into_push_pull_output();
+        led.set_high();
 
         // Configure TIM2 as a periodic timer
-        let mut timer = Timer::new(device.TIM2, &clocks);
+        let mut timer = Timer::new(device.TIM2, &clocks).counter_hz();
+        timer.start(1u32.Hz()).unwrap();
+
         timer.listen(Event::Update);
 
         (Shared {}, Local { led, timer }, init::Monotonics())
@@ -54,6 +61,7 @@ mod app {
 
     #[task(binds = TIM2, local = [led, timer])]
     fn blink(ctx: blink::Context) {
+        rprintln!("tick");
         let led = ctx.local.led;
         let timer = ctx.local.timer;
 
